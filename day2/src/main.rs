367753067e593e53e8e1c05630e9663913b544c5
Stijn Hoop <stijn.hoop@vanderlande.com>
2020-12-02 14:54:19 +0100

    Day 2 Star 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -9,6 +9,13 @@ where P: AsRef<Path>, {
     Ok(io::BufReader::new(file).lines())
 }
 
+fn crop_letters(s: &str, pos: usize) -> &str {
+    match s.char_indices().skip(pos).next() {
+        Some((pos, _)) => &s[pos..],
+        None => "",
+    }
+}
+
 struct Entry {
     lower: usize,
     upper: usize,
@@ -23,7 +30,7 @@ fn main() {
             if let Ok(rawrule) = line {
                 let mut rulevec: Vec<&str> = rawrule.split(":").collect();
                 assert_eq!(rulevec.len(), 2);
-                let pass = rulevec.pop().unwrap();
+                let pass = crop_letters(rulevec.pop().unwrap(), 1);
                 let rule = rulevec.pop().unwrap();
                 let mut boundsvec: Vec<&str> = rule.split(" ").collect();
                 assert_eq!(boundsvec.len(), 2);
@@ -47,7 +54,7 @@ fn main() {
     println!("Star 1:");
     let mut valid: usize = 0;
     let mut invalid: usize = 0;
-    for entry in entries {
+    for entry in &entries {
         let count = entry.pass.matches(entry.incl).count();
         if count >= entry.lower && count <= entry.upper {
             println!("VALID: pass {} char {} count {} between {}-{}", entry.pass, entry.incl, entry.pass.matches(entry.incl).count(), entry.lower, entry.upper);
@@ -58,4 +65,20 @@ fn main() {
         }
     }
     println!("VALID: {}, INVALID: {}", valid, invalid);
+
+    println!("Star 2:");
+    let mut valid2: usize = 0;
+    let mut invalid2: usize = 0;
+    for entry in &entries {
+        let match1 = entry.pass.chars().nth(entry.lower - 1).unwrap() == entry.incl;
+        let match2 = entry.pass.chars().nth(entry.upper - 1).unwrap() == entry.incl;
+        if (match1 && !match2) || (!match1 && match2) {  
+            println!("VALID: pass {} char {} positions {} and {}", entry.pass, entry.incl, entry.lower, entry.upper);
+            valid2 = valid2 + 1;
+        } else {
+            println!("INVALID: pass {} char {} positions {} and {}", entry.pass, entry.incl, entry.lower, entry.upper);
+            invalid2 = invalid2 + 1;
+        }
+    }
+    println!("VALID: {}, INVALID: {}", valid2, invalid2);
 }
