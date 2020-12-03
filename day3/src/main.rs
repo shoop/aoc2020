f6ec8621bfdc717a739b8624ff8de01f5b2581f1
Stijn Hoop <stijn.hoop@vanderlande.com>
2020-12-03 21:11:01 +0100

    Fun with iterators, however incorrect result...

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -3,41 +3,15 @@ use std::fs::File;
 use std::io::{self, BufRead};
 use std::vec::Vec;
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug)]
 struct IncorrectMapTileError;
 
-type Processor<S, E> = fn(&str, S) -> Result<S, E>;
-
-fn process_lines<S, E>(
-    iter: impl Iterator<Item = std::string::String>,
-    processor: Processor<S, E>,
-    initial: S,
-) -> Result<S, E> {
-    let mut result = initial;
-    for read in iter {
-        result = processor(&read, result)?;
-    }
-    Ok(result)
-}
-
-fn process_lines_from_file<S, E>(
-    file: &std::fs::File,
-    processor: Processor<S, E>,
-    initial: S,
-) -> Result<S, E> {
-    let iter = io::BufReader::new(file)
-        .lines()
-        .map(|x| x.expect("Could not read line"));
-    process_lines(iter, processor, initial)
-}
-
-#[derive(Debug, Copy, Clone, PartialEq)]
+#[derive(Debug, Clone, Copy, PartialEq)]
 enum MapTile {
     Empty,
     Tree,
 }
 
-#[derive(Debug)]
 struct Map {
     tiles: Vec<MapTile>,
     width: usize,
@@ -48,66 +22,111 @@ impl Map {
     fn tile_at(&self, x: usize, y: usize) -> MapTile {
         self.tiles[((y % self.height) * self.width) + (x % self.width)]
     }
-}
 
-fn parse_map(line: &str, mut map: Map) -> Result<Map, IncorrectMapTileError> {
-    for c in line.chars() {
-        if c == '.' {
-            map.tiles.push(MapTile::Empty);
-        } else if c == '#' {
-            map.tiles.push(MapTile::Tree);
-        } else {
-            return Err(IncorrectMapTileError);
+    fn slope(&self, incr_x: usize, incr_y: usize) -> Slope<'_> {
+        Slope {
+            map: &self,
+            incr_x,
+            incr_y,
         }
     }
+}
+
+struct Slope<'a> {
+    map: &'a Map,
+    incr_x: usize,
+    incr_y: usize,
+}
+
+impl<'a> IntoIterator for Slope<'a> {
+    type Item = MapTile;
+    type IntoIter = SlopeIntoIterator<'a>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        SlopeIntoIterator {
+            slope: self,
+            cur_x: 0,
+            cur_y: 0,
+        }
+    }
+}
+
+struct SlopeIntoIterator<'a> {
+    slope: Slope<'a>,
+    cur_x: usize,
+    cur_y: usize,
+}
+
+impl Iterator for SlopeIntoIterator<'_> {
+    type Item = MapTile;
+
+    fn next(&mut self) -> Option<MapTile> {
+        if self.cur_y > self.slope.map.height {
+            return None;
+        }
+
+        let result = self.slope.map.tile_at(self.cur_x, self.cur_y);
+        self.cur_x += self.slope.incr_x;
+        self.cur_y += self.slope.incr_y;
+
+        Some(result)
+    }
+}
+
+fn parse_map(mut map: Map, line: String) -> Result<Map, IncorrectMapTileError> {
+    for c in line.chars() {
+        match c {
+            '.' => map.tiles.push(MapTile::Empty),
+            '#' => map.tiles.push(MapTile::Tree),
+            _ => return Err(IncorrectMapTileError),
+        }
+    }
+
     if map.width == 0 {
         map.width = line.chars().count();
     }
-    map.height = map.height + 1;
+
+    map.height += 1;
 
     Ok(map)
 }
 
-fn check_slope(map: &Map, incr_x: usize, incr_y: usize) -> u32 {
-    let mut x: usize = 0;
-    let mut y: usize = 0;
-    let mut tree_count: u32 = 0;
-
-    while y < map.height {
-        if map.tile_at(x, y) == MapTile::Tree {
-            tree_count += 1;
-        }
-        x += incr_x;
-        y += incr_y;
-    }
-
-    tree_count
+fn check_slope<'a>(slope: Slope<'a>) -> u32 {
+    slope.into_iter().fold(0u32, |s, x| match x {
+        MapTile::Tree => s + 1,
+        MapTile::Empty => s,
+    })
 }
 
 fn star_one(map: &Map) -> u32 {
-    check_slope(map, 3, 1)
+    check_slope(map.slope(3, 1))
 }
 
 fn star_two(map: &Map) -> u32 {
-    check_slope(map, 1, 1)
-        * check_slope(map, 3, 1)
-        * check_slope(map, 5, 1)
-        * check_slope(map, 7, 1)
-        * check_slope(map, 1, 2)
+    let slopes = vec![
+        map.slope(1, 1),
+        map.slope(3, 1),
+        map.slope(5, 1),
+        map.slope(7, 1),
+        map.slope(1, 2),
+    ];
+    slopes.into_iter().fold(1, |s, x| s * check_slope(x))
 }
 
 fn main() {
     let file = File::open("./input").expect("Unreadable input file ./input");
-    let map = process_lines_from_file(
-        &file,
-        parse_map,
-        Map {
-            tiles: Vec::new(),
-            width: 0,
-            height: 0,
-        },
-    )
-    .expect("Could not parse map");
+    let map = io::BufReader::new(file)
+        .lines()
+        .map(|x| x.expect("Could not read line"))
+        .try_fold(
+            Map {
+                tiles: Vec::new(),
+                width: 0,
+                height: 0,
+            },
+            parse_map,
+        )
+        .expect("Invalid data in input file");
 
     println!("Star 1:");
     let nr_trees = star_one(&map);
@@ -120,9 +139,6 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
-    use super::Map;
-    use super::MapTile;
-
     static TEST_MAP: &str = "..##.......
 #...#...#..
 .#....#..#.
@@ -137,23 +153,25 @@ mod tests {
 
     #[test]
     fn test_star_one() {
-        let map = super::process_lines(
-            TEST_MAP.lines().map(|x| x.to_string()),
-            super::parse_map,
-            Map {
-                tiles: Vec::new(),
-                width: 0,
-                height: 0,
-            },
-        )
-        .expect("Invalid test data");
+        let map = TEST_MAP
+            .lines()
+            .map(|x| x.to_string())
+            .try_fold(
+                super::Map {
+                    tiles: Vec::new(),
+                    width: 0,
+                    height: 0,
+                },
+                super::parse_map,
+            )
+            .expect("Invalid test data");
 
         assert_eq!(map.width, 11);
         assert_eq!(map.height, 11);
 
-        assert_eq!(map.tile_at(0, 0), MapTile::Empty);
-        assert_eq!(map.tile_at(0, 1), MapTile::Tree);
-        assert_eq!(map.tile_at(11, 12), MapTile::Tree);
+        assert_eq!(map.tile_at(0, 0), super::MapTile::Empty);
+        assert_eq!(map.tile_at(0, 1), super::MapTile::Tree);
+        assert_eq!(map.tile_at(11, 12), super::MapTile::Tree);
 
         let nr_trees = super::star_one(&map);
         assert_eq!(nr_trees, 7);
@@ -161,20 +179,25 @@ mod tests {
 
     #[test]
     fn test_star_two() {
-        let mut map = Map {
-            tiles: Vec::new(),
-            width: 0,
-            height: 0,
-        };
-        for line in TEST_MAP.lines() {
-            map = super::parse_map(line, map).expect("Invalid test data");
-        }
+        let map = TEST_MAP
+            .lines()
+            .map(|x| x.to_string())
+            .try_fold(
+                super::Map {
+                    tiles: Vec::new(),
+                    width: 0,
+                    height: 0,
+                },
+                super::parse_map,
+            )
+            .expect("Invalid test data");
+
         assert_eq!(map.width, 11);
         assert_eq!(map.height, 11);
 
-        assert_eq!(map.tile_at(0, 0), MapTile::Empty);
-        assert_eq!(map.tile_at(0, 1), MapTile::Tree);
-        assert_eq!(map.tile_at(11, 12), MapTile::Tree);
+        assert_eq!(map.tile_at(0, 0), super::MapTile::Empty);
+        assert_eq!(map.tile_at(0, 1), super::MapTile::Tree);
+        assert_eq!(map.tile_at(11, 12), super::MapTile::Tree);
 
         let nr_trees = super::star_two(&map);
         assert_eq!(nr_trees, 336);
