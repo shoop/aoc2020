6587cb0183a05a474c6f114d1c40c7434be3bfc0
Stijn Hoop <stijn.hoop@vanderlande.com>
2020-12-03 10:32:41 +0100

    Add tests for day 2 and refactor as well

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -4,7 +4,9 @@ use std::path::Path;
 use std::vec::Vec;
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where P: AsRef<Path>, {
+where
+    P: AsRef<Path>,
+{
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
 }
@@ -16,6 +18,34 @@ fn crop_letters(s: &str, pos: usize) -> &str {
     }
 }
 
+fn parse_lines(lines: io::Lines<io::BufReader<File>>) -> Vec<Entry> {
+    let mut entries: Vec<Entry> = Vec::new();
+    for line in lines {
+        if let Ok(rawrule) = line {
+            let mut rulevec: Vec<&str> = rawrule.split(":").collect();
+            assert_eq!(rulevec.len(), 2);
+            let pass = crop_letters(rulevec.pop().unwrap(), 1);
+            let rule = rulevec.pop().unwrap();
+            let mut boundsvec: Vec<&str> = rule.split(" ").collect();
+            assert_eq!(boundsvec.len(), 2);
+            let incl = boundsvec.pop().unwrap().chars().next().unwrap();
+            let bounds = boundsvec.pop().unwrap();
+            let mut rangevec: Vec<&str> = bounds.split("-").collect();
+            assert_eq!(rangevec.len(), 2);
+            let upper: usize = rangevec.pop().unwrap().parse().unwrap();
+            let lower: usize = rangevec.pop().unwrap().parse().unwrap();
+
+            entries.push(Entry {
+                lower: lower,
+                upper: upper,
+                incl: incl,
+                pass: pass.to_string(),
+            })
+        }
+    }
+    entries
+}
+
 #[derive(Debug)]
 struct Entry {
     lower: usize,
@@ -24,64 +54,105 @@ struct Entry {
     pass: String,
 }
 
-fn main() {
-    let mut entries: Vec<Entry> = Vec::new();
-    if let Ok(lines) = read_lines("./input") {
-        for line in lines {
-            if let Ok(rawrule) = line {
-                let mut rulevec: Vec<&str> = rawrule.split(":").collect();
-                assert_eq!(rulevec.len(), 2);
-                let pass = crop_letters(rulevec.pop().unwrap(), 1);
-                let rule = rulevec.pop().unwrap();
-                let mut boundsvec: Vec<&str> = rule.split(" ").collect();
-                assert_eq!(boundsvec.len(), 2);
-                let incl = boundsvec.pop().unwrap().chars().next().unwrap();
-                let bounds = boundsvec.pop().unwrap();
-                let mut rangevec: Vec<&str> = bounds.split("-").collect();
-                assert_eq!(rangevec.len(), 2);
-                let upper: usize = rangevec.pop().unwrap().parse().unwrap();
-                let lower: usize = rangevec.pop().unwrap().parse().unwrap();
-
-                entries.push(Entry {
-                    lower: lower,
-                    upper: upper,
-                    incl: incl,
-                    pass: pass.to_string(),
-                })
-            }
-        }
-    }
-
-    println!("Star 1:");
+fn star_one(entries: &Vec<Entry>) -> (usize, usize) {
     let mut valid: usize = 0;
     let mut invalid: usize = 0;
-    for entry in &entries {
-        print!("{:?}: ", entry);
+    for entry in entries {
         let count = entry.pass.matches(entry.incl).count();
         if count >= entry.lower && count <= entry.upper {
-            println!("count {} VALID!", count);
             valid = valid + 1;
         } else {
-            println!("count {} INVALID!", count);
             invalid = invalid + 1;
         }
     }
+    (valid, invalid)
+}
+
+fn star_two(entries: &Vec<Entry>) -> (usize, usize) {
+    let mut valid: usize = 0;
+    let mut invalid: usize = 0;
+    for entry in entries {
+        let match1 = entry.pass.chars().nth(entry.lower - 1).unwrap() == entry.incl;
+        let match2 = entry.pass.chars().nth(entry.upper - 1).unwrap() == entry.incl;
+        if (match1 && !match2) || (!match1 && match2) {
+            valid = valid + 1;
+        } else {
+            invalid = invalid + 1;
+        }
+    }
+    (valid, invalid)
+}
+
+fn main() {
+    let lines = read_lines("./input").expect("Could not read input file ./input");
+    let entries = parse_lines(lines);
+
+    println!("Star 1:");
+    let (valid, invalid) = star_one(&entries);
     println!("VALID: {}, INVALID: {}", valid, invalid);
 
     println!("Star 2:");
-    let mut valid2: usize = 0;
-    let mut invalid2: usize = 0;
-    for entry in &entries {
-        print!("{:?}: ", entry);
-        let match1 = entry.pass.chars().nth(entry.lower - 1).unwrap() == entry.incl;
-        let match2 = entry.pass.chars().nth(entry.upper - 1).unwrap() == entry.incl;
-        if (match1 && !match2) || (!match1 && match2) {  
-            println!("VALID!");
-            valid2 = valid2 + 1;
-        } else {
-            println!("INVALID!");
-            invalid2 = invalid2 + 1;
-        }
+    let (valid, invalid) = star_two(&entries);
+    println!("VALID: {}, INVALID: {}", valid, invalid);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Entry;
+
+    #[test]
+    fn test_star_one() {
+        let entries = vec![
+            Entry {
+                lower: 1,
+                upper: 3,
+                incl: 'a',
+                pass: String::from("abcde"),
+            },
+            Entry {
+                lower: 1,
+                upper: 3,
+                incl: 'b',
+                pass: String::from("cdefg"),
+            },
+            Entry {
+                lower: 2,
+                upper: 9,
+                incl: 'c',
+                pass: String::from("ccccccccc"),
+            },
+        ];
+
+        let (valid, invalid) = super::star_one(&entries);
+        assert_eq!(valid, 2);
+        assert_eq!(invalid, 1);
     }
-    println!("VALID: {}, INVALID: {}", valid2, invalid2);
+
+    #[test]
+    fn test_star_two() {
+        let entries = vec![
+            Entry {
+                lower: 1,
+                upper: 3,
+                incl: 'a',
+                pass: String::from("abcde"),
+            },
+            Entry {
+                lower: 1,
+                upper: 3,
+                incl: 'b',
+                pass: String::from("cdefg"),
+            },
+            Entry {
+                lower: 2,
+                upper: 9,
+                incl: 'c',
+                pass: String::from("ccccccccc"),
+            },
+        ];
+
+        let (valid, invalid) = super::star_two(&entries);
+        assert_eq!(valid, 1);
+        assert_eq!(invalid, 2);
+    }
 }
