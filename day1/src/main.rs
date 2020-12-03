9a071efcb95542f0c1c30fb206c20beb5d0fe312
Stijn Hoop <stijn.hoop@vanderlande.com>
2020-12-03 09:30:24 +0100

    Refactory day 1 into fns

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -9,39 +9,50 @@ where P: AsRef<Path>, {
     Ok(io::BufReader::new(file).lines())
 }
 
-fn main() {
-    let mut vec: Vec<i32> = Vec::new();
-    if let Ok(lines) = read_lines("./input") {
-        for line in lines {
-            if let Ok(number) = line {
-                let parsed: i32 = number.parse().unwrap();
-                vec.push(parsed);
-            }
+fn parse_numbers(lines: io::Lines<io::BufReader<File>>) -> Vec<i32> {
+    let mut numbers: Vec<i32> = Vec::new();
+    for line in lines {
+        if let Ok(number) = line {
+            let parsed: i32 = number.parse().unwrap();
+            numbers.push(parsed);
         }
     }
+    numbers
+}
 
-    println!("Star 1:");
-    for (i, first) in vec.iter().enumerate() {
+fn star_one(numbers: &Vec<i32>) -> (i32, i32) {
+    for (i, first) in numbers.iter().enumerate() {
         let mut j = i + 1;
-        while j < vec.len() {
-            if first + vec[j] == 2020 {
-                println!("{} + {} = {}", first, vec[j], first + vec[j]);
-                println!("{} * {} = {}", first, vec[j], first * vec[j]);
+        while j < numbers.len() {
+            if first + numbers[j] == 2020 {
+                return (*first, numbers[j]);
             }
             j += 1;
         }
     }
+    (-1, -1)
+}
+
+fn main() {
+    let lines = read_lines("./input")
+        .expect("Could not read input file ./input");
+    let numbers = parse_numbers(lines);
+
+    println!("Star 1:");
+    let (num1, num2) = star_one(&numbers);
+    println!("{} + {} = {}", num1, num2, num1 + num2);
+    println!("{} * {} = {}", num1, num2, num1 * num2);
 
     println!("Star 2:");
     let mut i = 0;
-    while i < vec.len() {
+    while i < numbers.len() {
         let mut j = i + 1;
-        while j < vec.len() {
+        while j < numbers.len() {
             let mut k = j + 1;
-            while k < vec.len() {
-                if vec[i] + vec[j] + vec[k] == 2020 {
-                    println!("{} + {} + {} = {}", vec[i], vec[j], vec[k], vec[i] + vec[j] + vec[k]);
-                    println!("{} * {} * {} = {}", vec[i], vec[j], vec[k], vec[i] * vec[j] * vec[k]);
+            while k < numbers.len() {
+                if numbers[i] + numbers[j] + numbers[k] == 2020 {
+                    println!("{} + {} + {} = {}", numbers[i], numbers[j], numbers[k], numbers[i] + numbers[j] + numbers[k]);
+                    println!("{} * {} * {} = {}", numbers[i], numbers[j], numbers[k], numbers[i] * numbers[j] * numbers[k]);
                 }
                 k += 1;
             }
@@ -50,3 +61,11 @@ fn main() {
         i += 1;
     }
 }
+
+// #[cfg(test)]
+// mod tests {
+//     #[test]
+//     fn it_works() {
+//         assert_eq!(2 + 2, 4);
+//     }
+// }
