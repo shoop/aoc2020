efb3eebbd3a921f2791661f0069d51c61662fd46
Stijn Hoop <stijn.hoop@vanderlande.com>
2020-12-03 10:13:51 +0100

    Use error handling instead of sentinels

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -3,6 +3,9 @@ use std::io::{self, BufRead};
 use std::path::Path;
 use std::vec::Vec;
 
+#[derive(Debug)]
+struct DayOneError;
+
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
 where
     P: AsRef<Path>,
@@ -22,20 +25,20 @@ fn parse_numbers(lines: io::Lines<io::BufReader<File>>) -> Vec<i32> {
     numbers
 }
 
-fn star_one(numbers: &Vec<i32>) -> (i32, i32) {
+fn star_one(numbers: &Vec<i32>) -> Result<(i32, i32), DayOneError> {
     for (i, first) in numbers.iter().enumerate() {
         let mut j = i + 1;
         while j < numbers.len() {
             if first + numbers[j] == 2020 {
-                return (*first, numbers[j]);
+                return Ok((*first, numbers[j]));
             }
             j += 1;
         }
     }
-    (-1, -1)
+    Err(DayOneError)
 }
 
-fn star_two(numbers: &Vec<i32>) -> (i32, i32, i32) {
+fn star_two(numbers: &Vec<i32>) -> Result<(i32, i32, i32), DayOneError> {
     let mut i = 0;
     while i < numbers.len() {
         let mut j = i + 1;
@@ -43,7 +46,7 @@ fn star_two(numbers: &Vec<i32>) -> (i32, i32, i32) {
             let mut k = j + 1;
             while k < numbers.len() {
                 if numbers[i] + numbers[j] + numbers[k] == 2020 {
-                    return (numbers[i], numbers[j], numbers[k]);
+                    return Ok((numbers[i], numbers[j], numbers[k]));
                 }
                 k += 1;
             }
@@ -51,7 +54,7 @@ fn star_two(numbers: &Vec<i32>) -> (i32, i32, i32) {
         }
         i += 1;
     }
-    (-1, -1, -1)
+    Err(DayOneError)
 }
 
 
@@ -60,12 +63,12 @@ fn main() {
     let numbers = parse_numbers(lines);
 
     println!("Star 1:");
-    let (num1, num2) = star_one(&numbers);
+    let (num1, num2) = star_one(&numbers).expect("Invalid input data");
     println!("{} + {} = {}", num1, num2, num1 + num2);
     println!("{} * {} = {}", num1, num2, num1 * num2);
 
     println!("Star 2:");
-    let (num1, num2, num3) = star_two(&numbers);
+    let (num1, num2, num3) = star_two(&numbers).expect("Invalid input data");
     println!("{} + {} + {} = {}", num1, num2, num3, num1 + num2 + num3);
     println!("{} * {} * {} = {}", num1, num2, num3, num1 * num2 * num3);
 }
@@ -75,7 +78,7 @@ mod tests {
     #[test]
     fn test_star_one() {
         let numbers = vec![1721, 979, 366, 299, 675, 1456];
-        let (num1, num2) = super::star_one(&numbers);
+        let (num1, num2) = super::star_one(&numbers).expect("Invalid test data");
         assert_eq!(num1 + num2, 2020);
         assert_eq!(num1 * num2, 514579);
     }
@@ -83,7 +86,7 @@ mod tests {
     #[test]
     fn test_star_two() {
         let numbers = vec![1721, 979, 366, 299, 675, 1456];
-        let (num1, num2, num3) = super::star_two(&numbers);
+        let (num1, num2, num3) = super::star_two(&numbers).expect("Invalid test data");
         assert_eq!(num1 + num2 + num3, 2020);
         assert_eq!(num1 * num2 * num3, 241861950);
     }
