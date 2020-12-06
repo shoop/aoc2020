01d3ddd779076a5da1c5d786bb02604197aea163
Stijn Hoop <stijn@sandcat.nl>
2020-12-06 16:39:01 +0100

    Day 6 Star Two

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashSet;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::vec::Vec;
@@ -38,6 +39,34 @@ where
     ans
 }
 
+fn star_two<I>(iter: I) -> usize
+where
+    I: Iterator<Item = String>,
+{
+    let mut group_nr: usize = 0;
+    let group_answers: Vec<(usize, String)> = iter
+        .map(|x| {
+            if x.is_empty() {
+                group_nr += 1
+            }
+            (group_nr, x)
+        })
+        .filter(|x| !x.1.is_empty())
+        .collect();
+
+    let mut group_hash: Vec<HashSet<char>> = vec![];
+    for (g, v) in group_answers {
+        match group_hash.get(g) {
+            None => group_hash.push(v.chars().collect()),
+            _ => group_hash[g] = group_hash[g].intersection(&v.chars().collect()).copied().collect()
+        }
+    }
+
+    group_hash.iter().fold(0, |s, x| {
+        s + x.len()
+    })
+}
+
 fn main() {
     let file = File::open("./input").expect("Unreadable input file ./input");
     let lines = io::BufReader::new(file)
@@ -46,6 +75,13 @@ fn main() {
 
     let ans = star_one(lines);
     println!("Star one: {}", ans);
+
+    let file = File::open("./input").expect("Unreadable input file ./input");
+    let lines = io::BufReader::new(file)
+        .lines()
+        .map(|x| x.expect("Could not read line"));
+    let ans = star_two(lines);
+    println!("Star two: {}", ans);
 }
 
 #[cfg(test)]
@@ -71,5 +107,12 @@ b";
         let lines = TEST_DATA.lines().map(|x| String::from(x));
         let ans = super::star_one(lines);
         assert_eq!(ans, 11);
+    }
+
+    #[test]
+    fn test_star_two() {
+        let lines = TEST_DATA.lines().map(|x| String::from(x));
+        let ans = super::star_two(lines);
+        assert_eq!(ans, 6);
     }
 }
