c8bfcc24882decc23ed900a2410e72cd649af8fc
Stijn Hoop <stijn@sandcat.nl>
2020-12-06 15:58:17 +0100

    Single iterator version

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,72 +1,50 @@
-// use std::collections::HashSet;
-// use core::fmt::Debug;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::vec::Vec;
 
-// #[derive(Debug)]
-// struct IncorrectFormLine;
-
-// #[derive(Debug)]
-// struct Solution {
-//     groups: Vec<HashSet<char>>
-// }
-
-// impl Solution {
-    // fn new() -> Self {
-    //     Solution { groups: vec![] }
-    // }
-
-    // fn add_answers_for_group(self, form: &str) -> Result<Solution, IncorrectFormLine> {
-    //     let answers: HashSet<char> = HashSet::new();
-    //     for ch in form.chars() {
-    //         if ch == '\r' || ch == '\n' {
-    //             continue;
-    //         }
-    //         if !ch.is_alphabetic() || !ch.is_lowercase() {
-    //             return Err(IncorrectFormLine);
-    //         }
-
-    //         answers.insert(ch);
-    //     }
-    //     self.groups.push(answers);
-
-    //     Ok(self)
-    // }
-// }
-
-fn main() {
-    let file = File::open("./input").expect("Unreadable input file ./input");
-    let mut s: usize = 0;
-    let groups: Vec<(usize, String)> = io::BufReader::new(file)
-        .lines()
-        .map(|x| x.expect("Could not read line"))
+fn star_one<I>(iter: I) -> usize
+where
+    I: Iterator<Item = String>,
+{
+    let mut group_nr: usize = 0;
+    let mut group_answers: Vec<String> = vec![];
+    let ans = iter
         .map(|x| {
             if x.is_empty() {
-                s += 1
+                group_nr += 1
             }
-            (s, x)
+            (group_nr, x)
         })
         .filter(|x| !x.1.is_empty())
-        .collect();
-    let mut group_an: Vec<String> = vec![];
-    let group_an = groups.iter().fold(&mut group_an, |s, val| {
-        if let Some(_) = s.get_mut(val.0) {
-            s[val.0] = String::from(&s[val.0]) + &val.1;
-        } else {
-            s.push(String::from(&val.1));
-        }
+        .collect::<Vec<(usize, String)>>()
+        .iter()
+        .fold(&mut group_answers, |s, val| {
+            if let Some(_) = s.get_mut(val.0) {
+                s[val.0] = String::from(&s[val.0]) + &val.1;
+            } else {
+                s.push(String::from(&val.1));
+            }
 
-        s
-    });
-    let ans = group_an.iter()
+            s
+        })
+        .iter()
         .fold(0, |s, x| {
             let mut ch: Vec<char> = x.chars().collect();
             ch.sort();
             ch.dedup();
-            println!("{} unique {}", x, ch.len());
             s + ch.len()
         });
+
+    ans
+}
+
+fn main() {
+    let file = File::open("./input").expect("Unreadable input file ./input");
+    let lines = io::BufReader::new(file)
+        .lines()
+        .map(|x| x.expect("Could not read line"));
+
+    let ans = star_one(lines);
     println!("Star one: {}", ans);
 }
 
@@ -90,38 +68,8 @@ b";
 
     #[test]
     fn test_star_one() {
-        let mut s: usize = 0;
-        let groups: Vec<(usize, &str)> = TEST_DATA
-            .lines()
-            .map(|x| {
-                if x.is_empty() {
-                    s += 1
-                }
-                (s, x)
-            })
-            .filter(|x| !x.1.is_empty())
-            .collect();
-        println!("{:?} {}", groups, groups.len());
-        let mut group_an: Vec<String> = vec![];
-        let group_an = groups.iter().fold(&mut group_an, |s, val| {
-            if let Some(_) = s.get_mut(val.0) {
-                s[val.0] = String::from(&s[val.0]) + val.1;
-            } else {
-                s.push(String::from(val.1));
-            }
-
-            s
-        });
-        println!("{:?}", group_an);
-        assert_eq!(group_an.len(), 5);
-        let ans = group_an.iter()
-            .fold(0, |s, x| {
-                let mut ch: Vec<char> = x.chars().collect();
-                ch.sort();
-                ch.dedup();
-                println!("{} unique {}", x, ch.len());
-                s + ch.len()
-            });
+        let lines = TEST_DATA.lines().map(|x| String::from(x));
+        let ans = super::star_one(lines);
         assert_eq!(ans, 11);
     }
 }
