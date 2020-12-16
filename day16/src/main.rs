2b67ba3ef674ae76cf6d8b51c6288cfbf577266c
Stijn Hoop <stijn@sandcat.nl>
2020-12-16 23:27:01 +0100

    Day 16 Star Two
    This took way too long and is probably way overengineered but I did not see another way.

--- day16/src/main.rs
+++ day16/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::ops::Range;
@@ -6,14 +7,15 @@ use std::vec::Vec;
 #[derive(Debug)]
 struct InvalidInputError;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Rule {
     name: String,
+    field_index: Option<usize>,
     range_1: Range<isize>,
     range_2: Range<isize>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Ticket {
     values: Vec<isize>,
 }
@@ -88,6 +90,7 @@ fn parse_rule(line: &str) -> Result<Rule, InvalidInputError> {
 
     Ok(Rule {
         name: String::from(name),
+        field_index: None,
         range_1,
         range_2,
     })
@@ -152,11 +155,11 @@ where
     Ok(train_tickets)
 }
 
-fn star_one(_train_tickets: &TrainTickets) -> isize {
+fn star_one(train_tickets: &TrainTickets) -> isize {
     let mut invalid_values: Vec<isize> = vec![];
-    for ticket in _train_tickets.nearby_tickets.iter() {
+    for ticket in train_tickets.nearby_tickets.iter() {
         for value in ticket.values.iter() {
-            let valid = _train_tickets.rules.iter().fold(false, |s, x| {
+            let valid = train_tickets.rules.iter().fold(false, |s, x| {
                 s || x.range_1.contains(value) || x.range_2.contains(value)
             });
             if !valid {
@@ -168,6 +171,70 @@ fn star_one(_train_tickets: &TrainTickets) -> isize {
     invalid_values.iter().fold(0, |s, x| s + x)
 }
 
+fn star_two(train_tickets: &TrainTickets, prefix: &str) -> isize {
+    // Assert that we have the same amount of rules as we have fields
+    assert_eq!(
+        train_tickets.rules.len(),
+        train_tickets.my_ticket.values.len()
+    );
+
+    let mut valid_tickets: Vec<Ticket> = vec![];
+    for ticket in train_tickets.nearby_tickets.iter() {
+        let valid = ticket.values.iter().fold(true, |s, x| {
+            // All values should test true
+            s &&
+                // ... when it matches at least one rule
+                train_tickets.rules.iter().fold(false, |s2, x2| {
+                    s2 || x2.range_1.contains(x) || x2.range_2.contains(x)
+                })
+        });
+        if valid {
+            valid_tickets.push(ticket.clone());
+        }
+    }
+
+    let mut possible_fields: HashMap<usize, Vec<usize>> = HashMap::new();
+
+    for (idx, rule) in train_tickets.rules.iter().enumerate() {
+        for field_index in 0..train_tickets.my_ticket.values.len() {
+            let mut valid = true;
+            for ticket in valid_tickets.iter() {
+                if !rule.range_1.contains(&ticket.values[field_index])
+                    && !rule.range_2.contains(&ticket.values[field_index])
+                {
+                    valid = false;
+                    break;
+                }
+            }
+
+            if valid {
+                // Rule is valid for this field index, store that fact and break to next rule
+                possible_fields.entry(field_index).or_insert(vec![]).push(idx);
+            }
+        }
+    }
+
+    let mut rule_mapping: HashMap<usize, usize> = HashMap::new();
+    for _ in 0..train_tickets.my_ticket.values.len() {
+        let candidate: Vec<(&usize, &Vec<usize>)> = possible_fields.iter().filter(|(_, v)| v.len() == 1).collect();
+        assert_eq!(candidate.len(), 1);
+        let field_index = candidate[0].0;
+        let rule_index = candidate[0].1[0];
+        *rule_mapping.entry(rule_index).or_default() = *field_index;
+        for (_, v) in possible_fields.iter_mut() {
+            v.retain(|&x| x != rule_index);
+        }
+    }
+
+    train_tickets.rules
+        .iter()
+        .enumerate()
+        .fold(1, |s, (idx, x)| match x.name.starts_with(prefix) {
+            true => s * train_tickets.my_ticket.values[rule_mapping[&idx]],
+            false => s,
+        })
+}
+
 fn main() {
     let file = File::open("./input").expect("Unreadable input file ./input");
     let mut lines = io::BufReader::new(file)
@@ -177,11 +244,14 @@ fn main() {
 
     let ans = star_one(&train_tickets);
     println!("Star one: {}", ans);
+
+    let ans = star_two(&train_tickets, "departure");
+    println!("Star two: {}", ans);
 }
 
 #[cfg(test)]
 mod tests {
-    static TEST_DATA: &str = "class: 1-3 or 5-7
+    static TEST_DATA_1: &str = "class: 1-3 or 5-7
 row: 6-11 or 33-44
 seat: 13-40 or 45-50
 
@@ -194,9 +264,21 @@ nearby tickets:
 55,2,20
 38,6,12";
 
+    static TEST_DATA_2: &str = "class: 0-1 or 4-19
+row: 0-5 or 8-19
+seat: 0-13 or 16-19
+
+your ticket:
+11,12,13
+
+nearby tickets:
+3,9,18
+15,1,5
+5,14,9";
+
     #[test]
     fn test_star_one() {
-        let mut lines = TEST_DATA.lines().map(|x| String::from(x));
+        let mut lines = TEST_DATA_1.lines().map(|x| String::from(x));
         let train_tickets = super::parse_lines(&mut lines).expect("Invalid input in file");
 
         assert_eq!(train_tickets.rules[0].name, "class");
@@ -210,5 +292,18 @@ nearby tickets:
 
         let ans = super::star_one(&train_tickets);
         assert_eq!(ans, 71);
+    }
+
+    #[test]
+    fn test_star_two() {
+        let mut lines = TEST_DATA_2.lines().map(|x| String::from(x));
+        let train_tickets = super::parse_lines(&mut lines).expect("Invalid input in file");
+
+        assert_eq!(train_tickets.rules[0].name, "class");
+        assert_eq!(train_tickets.rules[0].range_1, 0..2);
+        assert_eq!(train_tickets.rules[0].range_2, 4..20);
+
+        let ans = super::star_two(&train_tickets, "");
+        assert_eq!(ans, 1716);
     }
 }
